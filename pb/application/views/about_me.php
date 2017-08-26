6b31d642811387957e1daf77d5f3a064a147a9aa
hvillareal30 <huey.villareal@jmc.edu.ph>
2017-08-26 12:45:45 +0800

    Code Igniter Quiz 1

---
+++ pb/application/views/about_me.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Me</title>
+</head>
+<body>
+
+<br>
+<br>
+<br>
+<center>
+ <h1>About Myself</h1>
+ <br>
+ <br>
+
+ <h2>Name : Huey M. Vilalreal</h2>
+ <h2>Age : 22 years old</h2>
+ <h2>Birthday : April 30, 1995 </h2>
+ <h2>Address : San Antonio Village 2, Sasa, Davao City</h2>
+ <h2>City: Davao</h2>
+ <h2>Province: Davao del Sur</h2>
+ <h2>Hobbies : Yes </h2>
+ <br>
+ <br>
+ <br>
+ <br>
+ <h3><a href="javascript:window.history.go(-1);" style="text-decoration: none">Back</a>
+  </center>
+</body>
+</html>
---
+++ pb/application/views/my_contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contacts</title>
+</head>
+<body>
+
+<br>
+<br>
+<br>
+<center>
+<h2>Contact No. : 09991920293</h2>
+<h2>Mail : <a href="https://mail.google.com/mail/?view=cm&fs=1&to=huey.villareal@jmc.edu.ph&tf=1" style="text-decoration: none">huey.villareal@jmc.edu.ph</a></h2>
+<br>
+<br>
+<br>
+<h3><a href="javascript:window.history.go(-1);" style="text-decoration: none">Back</a></h3>
+</center>
+</body>
+</html>
